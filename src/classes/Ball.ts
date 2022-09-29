ba4f3d17ea9019fa09aa58403852cd14368c9e3c
Pau <103753396+pau-develop@users.noreply.github.com>
2022-09-29 15:39:24 +0200

    Created collision function

--- src/classes/Ball.ts
+++ src/classes/Ball.ts
@@ -3,8 +3,10 @@ class Ball {
   height;
   color;
   posX = 158;
-  posY = 300;
-  speed = 4;
+  posY = 100;
+  speed = 5;
+  directionY = 1;
+  directionX = 1;
   constructor(width: number = 5, height: number = 5, color: string = "#fff") {
     this.width = width;
     this.height = height;

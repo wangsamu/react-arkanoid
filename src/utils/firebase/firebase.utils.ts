b9856a80bd9de5db9d1cc5912ddf4fede3823cdf
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-09-29 12:54:09 +0200

    Firebase.utils initial boilerplate created

---
+++ src/utils/firebase/firebase.utils.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCK5jKfDH44vIG2PPShFq6Dxb8_VxbV5L8",
+  authDomain: "react-arkanoid.firebaseapp.com",
+  projectId: "react-arkanoid",
+  storageBucket: "react-arkanoid.appspot.com",
+  messagingSenderId: "484068023438",
+  appId: "1:484068023438:web:d35e3fdf5f25315e043ddf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

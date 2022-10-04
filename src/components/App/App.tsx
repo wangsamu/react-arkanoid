0b70b3eb7db84ed1a8348bd8416100c7993a2376
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-10-04 10:53:45 +0200

    Fixing SignIn bugs

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,23 @@
 import { useEffect } from "react";
-import { useAppDispatch } from "../../app/hooks";
-import { setCurrentUser } from "../../app/users/usersActions";
-import { createUserDocumentFromAuth, onAuthStateChangedListener } from "../../utils/firebase/firebaseUtils";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { setCurrentUser } from "../../app/slices/usersSlice";
+import { RootState } from "../../app/store";
+import {
+  createUserDocumentFromAuth,
+  onAuthStateChangedListener,
+} from "../../utils/firebase/firebaseUtils";
 import Game from "../Game/Game";
 import SignIn from "../SignIn/SignIn";
 import AppStyled from "./AppStyled";
 
 function App() {
+  const currentUser = useAppSelector(
+    (state: RootState) => state.users.currentUser
+  );
   const dispatch = useAppDispatch();
 
-
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user:object) => {
+    const unsubscribe = onAuthStateChangedListener((user: object) => {
       if (user) {
         createUserDocumentFromAuth(user);
       }
@@ -20,10 +26,11 @@ function App() {
     return unsubscribe;
   }, [dispatch]);
 
+  console.log(currentUser);
   return (
     <AppStyled className="wrap-container">
+      {currentUser ? <button>Sign out</button> : <SignIn />}
       <Game />
-      <SignIn />
     </AppStyled>
   );
 }
